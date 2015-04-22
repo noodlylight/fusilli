bc909f88a690aa546f709b1f22bb7f777e2b55a4
Michail Bitzes <noodlylight@noodlylight.com>
2015-04-22 03:41:34 +0300

    Port plugin fadedesktop to fusilli

--- plugins/fadedesktop.c
+++ plugins/fadedesktop.c
@@ -5,7 +5,9 @@
  * fadedesktop.c
  *
  * Copyright (c) 2006 Robert Carr <racarr@beryl-project.org>
- * 		         2007 Danny Baumann <maniac@beryl-project.org>
+ *                       2007 Danny Baumann <maniac@beryl-project.org>
+ * Copyright (c) 2015 Michail Bitzes <noodlylight@gmail.com>
+ *
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,14 +23,17 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <compiz-core.h>
-#include "fadedesktop_options.h"
+#include <fusilli-core.h>
 
 #include <math.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <X11/Xproto.h>
+
+static int bananaIndex;
+
+static int displayPrivateIndex;
 
 typedef enum
 {
@@ -52,9 +57,11 @@ typedef struct _FadeDesktopScreen
 	PaintWindowProc paintWindow;
 	EnterShowDesktopModeProc enterShowDesktopMode;
 	LeaveShowDesktopModeProc leaveShowDesktopMode;
-	
+
 	FadeDesktopState state;
 	int fadeTime;
+
+	CompMatch window_match;
 } FadeDesktopScreen;
 
 typedef struct _FadeDesktopWindow
@@ -65,76 +72,81 @@ typedef struct _FadeDesktopWindow
 	GLushort opacity;
 } FadeDesktopWindow;
 
-#define GET_FADEDESKTOP_DISPLAY(d)				     \
-    ((FadeDesktopDisplay *) (d)->base.privates[displayPrivateIndex].ptr)
+#define GET_FADEDESKTOP_DISPLAY(d) \
+	((FadeDesktopDisplay *) (d)->privates[displayPrivateIndex].ptr)
 
-#define FD_DISPLAY(d)			   \
-    FadeDesktopDisplay *fd = GET_FADEDESKTOP_DISPLAY (d)
+#define FD_DISPLAY(d) \
+	FadeDesktopDisplay *fd = GET_FADEDESKTOP_DISPLAY (d)
 
-#define GET_FADEDESKTOP_SCREEN(s, fd)					 \
-    ((FadeDesktopScreen *) (s)->base.privates[(fd)->screenPrivateIndex].ptr)
+#define GET_FADEDESKTOP_SCREEN(s, fd) \
+	((FadeDesktopScreen *) (s)->privates[(fd)->screenPrivateIndex].ptr)
 
-#define FD_SCREEN(s)							\
-    FadeDesktopScreen *fs = GET_FADEDESKTOP_SCREEN (s, GET_FADEDESKTOP_DISPLAY (s->display))
+#define FD_SCREEN(s) \
+	FadeDesktopScreen *fs = GET_FADEDESKTOP_SCREEN (s, GET_FADEDESKTOP_DISPLAY (&display))
 
-#define GET_FADEDESKTOP_WINDOW(w, fs)					 \
-    ((FadeDesktopWindow *) (w)->base.privates[(fs)->windowPrivateIndex].ptr)
+#define GET_FADEDESKTOP_WINDOW(w, fs) \
+	((FadeDesktopWindow *) (w)->privates[(fs)->windowPrivateIndex].ptr)
 
-#define FD_WINDOW(w)							\
-    FadeDesktopWindow *fw = GET_FADEDESKTOP_WINDOW(w, \
-					GET_FADEDESKTOP_SCREEN (w->screen, GET_FADEDESKTOP_DISPLAY (w->screen->display)))
-
-
-static int displayPrivateIndex;
-
+#define FD_WINDOW(w) \
+	FadeDesktopWindow *fw = GET_FADEDESKTOP_WINDOW (w, \
+	                                                GET_FADEDESKTOP_SCREEN (w->screen, GET_FADEDESKTOP_DISPLAY (&display)))
 
 static void
 fadeDesktopActivateEvent (CompScreen *s,
-						  Bool       activating)
+                          Bool       activating)
 {
-	CompOption o[2];
+	BananaArgument arg[2];
 
-	o[0].type = CompOptionTypeInt;
-	o[0].name = "root";
-	o[0].value.i = s->root;
+	arg[0].type = BananaInt;
+	arg[0].name = "root";
+	arg[0].value.i = s->root;
 
-	o[1].type = CompOptionTypeBool;
-	o[1].name = "active";
-	o[1].value.b = activating;
+	arg[1].type = BananaBool;
+	arg[1].name = "active";
+	arg[1].value.b = activating;
 
-	(*s->display->handleCompizEvent)
-		(s->display, "fadedesktop", "activate", o, 2);
+	(*display.handleFusilliEvent) ("fadedesktop", "activate", arg, 2);
 }
 
-static Bool isFDWin(CompWindow *w)
+static Bool
+isFDWin (CompWindow *w)
 {
+	FD_SCREEN (w->screen);
+
 	if (w->attrib.override_redirect)
 		return FALSE;
 
 	if (w->grabbed)
-	    return FALSE;
+		return FALSE;
 
 	if (!w->managed)
 		return FALSE;
 
 	if (w->wmType & (CompWindowTypeDesktopMask |
-			 CompWindowTypeDockMask))
-	    return FALSE;
+	                 CompWindowTypeDockMask))
+		return FALSE;
 
 	if (w->state & CompWindowStateSkipPagerMask)
-	    return FALSE;
+		return FALSE;
 
-	if (!matchEval(fadedesktopGetWindowMatch(w->screen), w))
+	if (!matchEval (&fs->window_match, w))
 		return FALSE;
 
 	return TRUE;
 }
 
-static void 
-fadeDesktopPreparePaintScreen(CompScreen *s, int msSinceLastPaint)
+static void
+fadeDesktopPreparePaintScreen (CompScreen *s,
+                               int        msSinceLastPaint)
 {
-	FD_SCREEN(s);
+	FD_SCREEN (s);
+
 	Bool doFade;
+
+	const BananaValue *
+	option_fade_time = bananaGetOption (bananaIndex,
+	                                    "fade_time",
+	                                    s->screenNum);
 
 	fs->fadeTime -= msSinceLastPaint;
 	if (fs->fadeTime < 0)
@@ -145,7 +157,7 @@ fadeDesktopPreparePaintScreen(CompScreen *s, int msSinceLastPaint)
 		CompWindow *w;
 		for (w = s->windows; w; w = w->next)
 		{
-			FD_WINDOW(w);
+			FD_WINDOW (w);
 
 			if (fs->state == FD_STATE_OUT)
 				doFade = fw->fading && w->inShowDesktopMode;
@@ -154,22 +166,23 @@ fadeDesktopPreparePaintScreen(CompScreen *s, int msSinceLastPaint)
 
 			if (doFade)
 			{
-			    fw->opacity = w->paint.opacity * 
-					  (float)((fs->state == FD_STATE_OUT) ? fs->fadeTime : 
-				          fadedesktopGetFadetime(s) - fs->fadeTime) / 
-				          (float)fadedesktopGetFadetime(s);
+				fw->opacity = w->paint.opacity *
+				              (float)((fs->state == FD_STATE_OUT) ? fs->fadeTime :
+				                      option_fade_time->i - fs->fadeTime) /
+				              (float)option_fade_time->i;
 			}
 		}
 	}
 
 	UNWRAP (fs, s, preparePaintScreen);
-	(*s->preparePaintScreen) (s, msSinceLastPaint);
+	(*s->preparePaintScreen)(s, msSinceLastPaint);
 	WRAP (fs, s, preparePaintScreen, fadeDesktopPreparePaintScreen);
 }
 
-static void fadeDesktopDonePaintScreen(CompScreen *s)
+static void
+fadeDesktopDonePaintScreen (CompScreen *s)
 {
-	FD_SCREEN(s);
+	FD_SCREEN (s);
 
 	if ((fs->state == FD_STATE_OUT) || (fs->state == FD_STATE_IN))
 	{
@@ -180,12 +193,13 @@ static void fadeDesktopDonePaintScreen(CompScreen *s)
 
 			for (w = s->windows; w; w = w->next)
 			{
-				FD_WINDOW(w);
+				FD_WINDOW (w);
+
 				if (fw->fading)
 				{
 					if (fs->state == FD_STATE_OUT)
 					{
-						hideWindow(w);
+						hideWindow (w);
 						fw->isHidden = TRUE;
 					}
 					fw->fading = FALSE;
@@ -198,10 +212,11 @@ static void fadeDesktopDonePaintScreen(CompScreen *s)
 				fs->state = FD_STATE_ON;
 			else
 				fs->state = FD_STATE_OFF;
+
 			fadeDesktopActivateEvent (s, FALSE);
 		}
 		else
-			damageScreen(s);
+			damageScreen (s);
 	}
 
 	UNWRAP (fs, s, donePaintScreen);
@@ -209,16 +224,17 @@ static void fadeDesktopDonePaintScreen(CompScreen *s)
 	WRAP (fs, s, donePaintScreen, fadeDesktopDonePaintScreen);
 }
 
-static Bool fadeDesktopPaintWindow (CompWindow *w, 
-				    const WindowPaintAttrib *attrib,
-				    const CompTransform *transform, 
-				    Region region,
-				    unsigned int mask)
+static Bool
+fadeDesktopPaintWindow (CompWindow              *w,
+                        const WindowPaintAttrib *attrib,
+                        const CompTransform     *transform,
+                        Region                  region,
+                        unsigned int            mask)
 {
 	Bool status;
 	CompScreen *s = w->screen;
-	FD_WINDOW(w);
-	FD_SCREEN(s);
+	FD_WINDOW (w);
+	FD_SCREEN (s);
 
 	if (fw->fading || fw->isHidden)
 	{
@@ -226,23 +242,30 @@ static Bool fadeDesktopPaintWindow (CompWindow *w,
 		wAttrib.opacity = fw->opacity;
 
 		UNWRAP (fs, s, paintWindow);
-		status = (*s->paintWindow) (w, &wAttrib, transform, region, mask);
+		status = (*s->paintWindow)(w, &wAttrib, transform, region, mask);
 		WRAP (fs, s, paintWindow, fadeDesktopPaintWindow);
 	}
 	else
 	{
 		UNWRAP (fs, s, paintWindow);
-		status = (*s->paintWindow) (w, attrib, transform, region, mask);
+		status = (*s->paintWindow)(w, attrib, transform, region, mask);
 		WRAP (fs, s, paintWindow, fadeDesktopPaintWindow);
 	}
 
 	return status;
 }
 
-static void fadeDesktopEnterShowDesktopMode(CompScreen *s)
+static void
+fadeDesktopEnterShowDesktopMode (CompScreen *s)
 {
-	FD_SCREEN(s);
+	FD_SCREEN (s);
+
 	CompWindow *w;
+
+	const BananaValue *
+	option_fade_time = bananaGetOption (bananaIndex,
+	                                    "fade_time",
+	                                    s->screenNum);
 
 	if ((fs->state == FD_STATE_OFF) || (fs->state == FD_STATE_IN))
 	{
@@ -250,13 +273,13 @@ static void fadeDesktopEnterShowDesktopMode(CompScreen *s)
 			fadeDesktopActivateEvent (s, TRUE);
 
 		fs->state = FD_STATE_OUT;
-		fs->fadeTime = fadedesktopGetFadetime(s) - fs->fadeTime;
+		fs->fadeTime = option_fade_time->i - fs->fadeTime;
 
 		for (w = s->windows; w; w = w->next)
 		{
-			if (isFDWin(w))
+			if (isFDWin (w))
 			{
-				FD_WINDOW(w);
+				FD_WINDOW (w);
 
 				fw->fading = TRUE;
 				w->inShowDesktopMode = TRUE;
@@ -264,17 +287,24 @@ static void fadeDesktopEnterShowDesktopMode(CompScreen *s)
 			}
 		}
 
-		damageScreen(s);
+		damageScreen (s);
 	}
 
-        UNWRAP(fs, s, enterShowDesktopMode);
-        (*s->enterShowDesktopMode)(s);
-        WRAP(fs, s, enterShowDesktopMode, fadeDesktopEnterShowDesktopMode);
+	UNWRAP (fs, s, enterShowDesktopMode);
+	(*s->enterShowDesktopMode)(s);
+	WRAP (fs, s, enterShowDesktopMode, fadeDesktopEnterShowDesktopMode);
 }
 
-static void fadeDesktopLeaveShowDesktopMode(CompScreen *s, CompWindow *w)
+static void
+fadeDesktopLeaveShowDesktopMode (CompScreen *s,
+                                 CompWindow *w)
 {
-	FD_SCREEN(s);
+	FD_SCREEN (s);
+
+	const BananaValue *
+	option_fade_time = bananaGetOption (bananaIndex,
+	                                    "fade_time",
+	                                    s->screenNum);
 
 	if (fs->state != FD_STATE_OFF)
 	{
@@ -286,7 +316,7 @@ static void fadeDesktopLeaveShowDesktopMode(CompScreen *s, CompWindow *w)
 				fadeDesktopActivateEvent (s, TRUE);
 
 			fs->state = FD_STATE_IN;
-			fs->fadeTime = fadedesktopGetFadetime(s) - fs->fadeTime;
+			fs->fadeTime = option_fade_time->i - fs->fadeTime;
 		}
 
 		for (cw = s->windows; cw; cw = cw->next)
@@ -294,11 +324,12 @@ static void fadeDesktopLeaveShowDesktopMode(CompScreen *s, CompWindow *w)
 			if (w && (w->id != cw->id))
 				continue;
 
-			FD_WINDOW(cw);
+			FD_WINDOW (cw);
+
 			if (fw->isHidden)
 			{
 				cw->inShowDesktopMode = FALSE;
-				showWindow(cw);
+				showWindow (cw);
 				fw->isHidden = FALSE;
 				fw->fading = TRUE;
 			}
@@ -308,167 +339,208 @@ static void fadeDesktopLeaveShowDesktopMode(CompScreen *s, CompWindow *w)
 			}
 		}
 
-		damageScreen(s);
+		damageScreen (s);
 	}
 
-       UNWRAP (fs, s, leaveShowDesktopMode); 
-        (*s->leaveShowDesktopMode)(s, w);
-        WRAP (fs, s, leaveShowDesktopMode, fadeDesktopLeaveShowDesktopMode);
+	UNWRAP (fs, s, leaveShowDesktopMode);
+	(*s->leaveShowDesktopMode) (s, w);
+	WRAP (fs, s, leaveShowDesktopMode, fadeDesktopLeaveShowDesktopMode);
 }
 
-static Bool fadeDesktopInit(CompPlugin *p)
-{
-	displayPrivateIndex = allocateDisplayPrivateIndex();
-	if (displayPrivateIndex < 0)
-		return FALSE;
-	
-	return TRUE;
-}
-
-
-static void fadeDesktopFini(CompPlugin *p)
-{
-	if (displayPrivateIndex >= 0)
-		freeDisplayPrivateIndex(displayPrivateIndex);
-}
-
-static Bool fadeDesktopInitDisplay(CompPlugin *p, CompDisplay *d)
+static Bool
+fadeDesktopInitDisplay (CompPlugin  *p,
+                        CompDisplay *d)
 {
 	FadeDesktopDisplay *fd;
-	if (!checkPluginABI ("core", CORE_ABIVERSION))
-	    return FALSE;
-	fd = malloc(sizeof(FadeDesktopDisplay));
+
+	fd = malloc (sizeof(FadeDesktopDisplay));
 	if (!fd)
 		return FALSE;
-	fd->screenPrivateIndex = allocateScreenPrivateIndex(d);
+
+	fd->screenPrivateIndex = allocateScreenPrivateIndex ();
 	if (fd->screenPrivateIndex < 0)
 	{
-		free(fd);
+		free (fd);
 		return FALSE;
 	}
-	
-	d->base.privates[displayPrivateIndex].ptr = fd;
+
+	d->privates[displayPrivateIndex].ptr = fd;
+
 	return TRUE;
 }
 
-static void fadeDesktopFiniDisplay(CompPlugin *p, CompDisplay *d)
+static void
+fadeDesktopFiniDisplay (CompPlugin  *p,
+                        CompDisplay *d)
 {
-	FD_DISPLAY(d);
-	freeScreenPrivateIndex(d, fd->screenPrivateIndex);
-	free(fd);
+	FD_DISPLAY (d);
+
+	freeScreenPrivateIndex (fd->screenPrivateIndex);
+
+	free (fd);
 }
 
-static Bool fadeDesktopInitScreen(CompPlugin *p, CompScreen *s)
+static Bool
+fadeDesktopInitScreen (CompPlugin *p,
+                       CompScreen *s)
 {
 	FadeDesktopScreen *fs;
-	FD_DISPLAY(s->display);
 
-	fs = malloc(sizeof(FadeDesktopScreen));
+	FD_DISPLAY (&display);
+
+	fs = malloc (sizeof (FadeDesktopScreen));
 	if (!fs)
 		return FALSE;
 
-	fs->windowPrivateIndex = allocateWindowPrivateIndex(s);	
+	fs->windowPrivateIndex = allocateWindowPrivateIndex (s);
 	if (fs->windowPrivateIndex < 0)
 	{
-		free(fs);
+		free (fs);
 		return FALSE;
 	}
 
 	fs->state = FD_STATE_OFF;
 	fs->fadeTime = 0;
 
-	WRAP(fs, s, paintWindow, fadeDesktopPaintWindow);
-	WRAP(fs, s, preparePaintScreen, fadeDesktopPreparePaintScreen);
-	WRAP(fs, s, donePaintScreen, fadeDesktopDonePaintScreen);
-	WRAP(fs, s, enterShowDesktopMode, fadeDesktopEnterShowDesktopMode);
-	WRAP(fs, s, leaveShowDesktopMode, fadeDesktopLeaveShowDesktopMode);
+	WRAP (fs, s, paintWindow, fadeDesktopPaintWindow);
+	WRAP (fs, s, preparePaintScreen, fadeDesktopPreparePaintScreen);
+	WRAP (fs, s, donePaintScreen, fadeDesktopDonePaintScreen);
+	WRAP (fs, s, enterShowDesktopMode, fadeDesktopEnterShowDesktopMode);
+	WRAP (fs, s, leaveShowDesktopMode, fadeDesktopLeaveShowDesktopMode);
 
-	s->base.privates[fd->screenPrivateIndex].ptr = fs;
+	const BananaValue *
+	option_window_match = bananaGetOption (bananaIndex,
+	                                       "window_match",
+	                                       s->screenNum);
+
+	matchInit (&fs->window_match);
+	matchAddFromString (&fs->window_match, option_window_match->s);
+	matchUpdate (&fs->window_match);
+
+	s->privates[fd->screenPrivateIndex].ptr = fs;
 
 	return TRUE;
-	
+
 }
 
-static void fadeDesktopFiniScreen(CompPlugin *p, CompScreen *s)
+static void
+fadeDesktopFiniScreen (CompPlugin *p,
+                       CompScreen *s)
 {
-	FD_SCREEN(s);
-	
-	UNWRAP(fs, s, paintWindow);
-	UNWRAP(fs, s, preparePaintScreen);
-	UNWRAP(fs, s, donePaintScreen);
-	UNWRAP(fs, s, enterShowDesktopMode);
-	UNWRAP(fs, s, leaveShowDesktopMode);
-	
-	freeWindowPrivateIndex(s, fs->windowPrivateIndex);
-	
+	FD_SCREEN (s);
+
+	matchFini (&fs->window_match);
+
+	UNWRAP (fs, s, paintWindow);
+	UNWRAP (fs, s, preparePaintScreen);
+	UNWRAP (fs, s, donePaintScreen);
+	UNWRAP (fs, s, enterShowDesktopMode);
+	UNWRAP (fs, s, leaveShowDesktopMode);
+
+	freeWindowPrivateIndex (s, fs->windowPrivateIndex);
+
 	free (fs);
 }
 
-static Bool fadeDesktopInitWindow(CompPlugin *p, CompWindow *w)
+static Bool
+fadeDesktopInitWindow (CompPlugin *p,
+                       CompWindow *w)
 {
 	FadeDesktopWindow *fw;
-	FD_SCREEN(w->screen);
 
-	fw = malloc(sizeof(FadeDesktopWindow));
+	FD_SCREEN (w->screen);
+
+	fw = malloc (sizeof (FadeDesktopWindow));
 	if (!fw)
 		return FALSE;
 
 	fw->isHidden = FALSE;
 	fw->fading = FALSE;
-	
-	w->base.privates[fs->windowPrivateIndex].ptr = fw;
+
+	w->privates[fs->windowPrivateIndex].ptr = fw;
 
 	return TRUE;
-	
-}
 
-static void fadeDesktopFiniWindow(CompPlugin *p, CompWindow *w)
-{
-	FD_WINDOW(w);
-	
-	free (fw);
-}
-
-static CompBool
-fadeDesktopInitObject (CompPlugin *p,
-		       CompObject *o)
-{
-	static InitPluginObjectProc dispTab[] = {
-		(InitPluginObjectProc) 0, /* InitCore */
-		(InitPluginObjectProc) fadeDesktopInitDisplay,
-		(InitPluginObjectProc) fadeDesktopInitScreen,
-		(InitPluginObjectProc) fadeDesktopInitWindow
-    };
-
-    RETURN_DISPATCH (o, dispTab, ARRAY_SIZE (dispTab), TRUE, (p, o));
 }
 
 static void
-fadeDesktopFiniObject (CompPlugin *p,
-		       CompObject *o)
+fadeDesktopFiniWindow (CompPlugin *p,
+                       CompWindow *w)
 {
-    static FiniPluginObjectProc dispTab[] = {
-		(FiniPluginObjectProc) 0, /* FiniCore */
-		(FiniPluginObjectProc) fadeDesktopFiniDisplay,
-		(FiniPluginObjectProc) fadeDesktopFiniScreen,
-		(FiniPluginObjectProc) fadeDesktopFiniWindow
-    };
+	FD_WINDOW (w);
 
-    DISPATCH (o, dispTab, ARRAY_SIZE (dispTab), (p, o));
+	free (fw);
+}
+
+static void
+fadeDesktopChangeNotify (const char        *optionName,
+                         BananaType        optionType,
+                         const BananaValue *optionValue,
+                         int               screenNum)
+{
+	if (strcasecmp (optionName, "window_match") == 0)
+	{
+		CompScreen *s = getScreenFromScreenNum (screenNum);
+		FD_SCREEN (s);
+
+		matchFini (&fs->window_match);
+		matchInit (&fs->window_match);
+		matchAddFromString (&fs->window_match, optionValue->s);
+		matchUpdate (&fs->window_match);
+	}
+}
+
+static Bool
+fadeDesktopInit (CompPlugin *p)
+{
+	if (getCoreABI() != CORE_ABIVERSION)
+	{
+		compLogMessage ("fadedesktop", CompLogLevelError,
+		                "ABI mismatch\n"
+		                "\tPlugin was compiled with ABI: %d\n"
+		                "\tFusilli Core was compiled with ABI: %d\n",
+		                CORE_ABIVERSION, getCoreABI());
+
+		return FALSE;
+	}
+
+	displayPrivateIndex = allocateDisplayPrivateIndex ();
+
+	if (displayPrivateIndex < 0)
+		return FALSE;
+
+	bananaIndex = bananaLoadPlugin ("fadedesktop");
+
+	if (bananaIndex == -1)
+		return FALSE;
+
+	bananaAddChangeNotifyCallBack (bananaIndex, fadeDesktopChangeNotify);
+
+	return TRUE;
+}
+
+static void
+fadeDesktopFini (CompPlugin *p)
+{
+	freeDisplayPrivateIndex (displayPrivateIndex);
+
+	bananaUnloadPlugin (bananaIndex);
 }
 
 static CompPluginVTable fadeDesktopVTable = {
 	"fadedesktop",
-	0,
 	fadeDesktopInit,
 	fadeDesktopFini,
-	fadeDesktopInitObject,
-	fadeDesktopFiniObject,
-	NULL,
-	NULL
+	fadeDesktopInitDisplay,
+	fadeDesktopFiniDisplay,
+	fadeDesktopInitScreen,
+	fadeDesktopFiniScreen,
+	fadeDesktopInitWindow,
+	fadeDesktopFiniWindow
 };
 
-CompPluginVTable * getCompPluginInfo(void)
+CompPluginVTable *
+getCompPluginInfo20141205 (void)
 {
 	return &fadeDesktopVTable;
 }
